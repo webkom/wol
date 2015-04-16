fc6a22b401df359355fb83de3f8fe54ed37dff04
ekmartin <ekmartinb@gmail.com>
2015-04-16 11:42:00 +0200

    Add error handling to file open

--- wol.py
+++ wol.py
@@ -3,17 +3,22 @@ import requests
 import logging
 import yaml
 import time
-
-conf = yaml.safe_load(open('config.yml'))
-
-target_servers = conf['servers']
+import os
 
 logger = logging.getLogger('wol')
+
+cwd = os.path.dirname(os.path.abspath(__file__))
+try:
+    conf = yaml.safe_load(open(cwd + '/config.yml'))
+except IOError:
+    logger.error('Can\'t find file config.yml')
+
+target_servers = conf['servers']
 
 resp = requests.get(conf['census_url'])
 
 for server in resp.json()['servers']:
     if server['hostname'] in target_servers.keys() and not server['isOk']:
-        print("%s is down, sending WOL" % server['hostname'])
+        logger.info("%s is down, sending WOL" % server['hostname'])
         wol.send_magic_packet(target_servers[server['hostname']])
         time.sleep(10)
